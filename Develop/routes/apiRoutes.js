517a87cd4547e9cbd34328353ddab5c136835f12
Billy Hao <billyhao12@gmail.com>
2020-06-17 16:35:14 -0700

    Now using separate files for html and api routes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const path = require("path");
+
+router.get("/notes", (req, res) => {
+    // TBD
+});
+
+router.post("/notes", (req, res) => {
+    // TBD
+});
+
+router.delete("/notes/:id", (req, res) => {
+    // TBD
+});
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,37 +1,15 @@
 const express = require("express");
-const path = require("path");
+
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/notes.html"));
-});
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/index.html"));
-});
-
-// API routes
-
-app.get("/api/notes", (req, res) => {
-    // TBD
-});
-
-app.post("/api/notes", (req, res) => {
-    // TBD
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-    // TBD
-});
+app.use(htmlRoutes);
+app.use("/api", apiRoutes);
 
 // Listener
 app.listen( PORT, () => {
