26ed2e5531155841a53e4c2402779b9b3ed2e605
Billy Hao <billyhao12@gmail.com>
2020-06-17 15:41:53 -0700

    Moved HTML pages from public directory to views directory

--- Develop/server.js
+++ Develop/server.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "views/notes.html"));
 });
 
 // Listener
